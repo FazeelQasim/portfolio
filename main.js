a4f1aff080e2d71640e59f4949bd0d158e932f2d
FazeelQasim <fazeelqasim2112@gmail.com>
2024-08-31 05:38:52 +0500

    header change and portfolio section

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav');
-const overlay = document.querySelector('.overlay');
+// const hamburger = document.querySelector('.hamburger');
+// const nav = document.querySelector('.nav');
+// const overlay = document.querySelector('.overlay');
 
-hamburger.addEventListener('click', () => {
-    nav.classList.toggle('show-menu');
-    overlay.classList.toggle('show-overlay');
-});
+// hamburger.addEventListener('click', () => {
+//     nav.classList.toggle('show-menu');
+//     overlay.classList.toggle('show-overlay');
+// });
 
-overlay.addEventListener('click', () => {
-    nav.classList.remove('show-menu');
-    overlay.classList.remove('show-overlay');
-});
+// overlay.addEventListener('click', () => {
+//     nav.classList.remove('show-menu');
+//     overlay.classList.remove('show-overlay');
+// });
+
+
+const header = document.querySelector('.styled-header');
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
+
+  menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    navLinks.classList.toggle('active');
+  });
